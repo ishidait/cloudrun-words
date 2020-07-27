882a30d954dd2fec1ae7adc3850d42bdb71704ee
Makoto Ishida <makotoishida@gmail.com>
2020-07-26 22:53:56 -1000

    Update index.js and app.css

--- index.js
+++ index.js
@@ -10,5 +10,14 @@ app.listen(port, () => {
 
 app.get('/hello', async (req, res) => {
   const word = req.query.w;
-  res.send(`Hello, you entered ${word}!`);
+  res.send(escaptHtml(`Hello, you entered ${word}!`));
 });
+
+function escaptHtml(str) {
+  const tagsToReplace = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+  };
+  return str.replace(/[&<>]/g, (tag) => tagsToReplace[tag] || tag);
+}
