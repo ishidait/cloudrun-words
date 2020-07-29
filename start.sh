8ba049289bf03f8dce3236f461456678ca9cc2e6
Makoto Ishida <makotoishida@gmail.com>
2020-07-28 22:19:57 -1000

    ローカル開発環境に.envファイルを追加。

--- start.sh
+++
@@ -1,7 +0,0 @@
-export GOOGLE_APPLICATION_CREDENTIALS=~/Development/Web/Practice/CloudRun-Words/cloudrun-words-aa3eb0379497.json
-export DB_USER=appuser
-export DB_PASS=apppwd
-export DB_NAME=words
-export DB_HOST=localhost
-
-npm run dev
