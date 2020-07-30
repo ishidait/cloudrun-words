39038e76b145a5d2b3b0d71985d2ddbbabb9e5d0
Makoto Ishida <makotoishida@gmail.com>
2020-07-29 21:59:58 -1000

    バックエンド側APIを実装。

---
+++ src/client/api.js
@@ -0,0 +1,76 @@
+/* 
+Parcelでasync/awaitを使うためには、package.jsonに
+  "browserslist": ["since 2017-06"]
+を追記する必要があるので注意。
+*/
+
+const API_URL = 'http://localhost:8080';
+
+async function executeApi(path, method = 'get', body = undefined) {
+  const response = await fetch(`${API_URL}${path}`, {
+    method,
+    mode: 'cors',
+    cache: 'no-cache',
+    headers: new Headers({ 'Content-Type': 'application/json' }),
+    body: body ? JSON.stringify(body) : undefined,
+  });
+  const result = await response.json();
+  return result;
+}
+
+export async function getWords() {
+  const result = await executeApi('/words');
+  if (result.status !== 'ok') {
+    throw new Error(`${result.status} ${result.data}`);
+  }
+  return result.data;
+}
+
+export function validate(word) {
+  if (!word.ja && !word.en && !word.es && !word.fr) {
+    return '何らかの単語・文を入力してください。';
+  }
+}
+
+export async function saveWord(word) {
+  const err = validate(word);
+  if (err) throw new Error(err);
+
+  const path = word.id ? `/words/${word.id}` : '/words';
+  const body = { ...word };
+  const result = await executeApi(path, word.id ? 'put' : 'post', body);
+  if (result.status !== 'ok') {
+    throw new Error(`${result.status} ${result.data}`);
+  }
+  return result.data;
+}
+
+export async function updateDone(word) {
+  const path = `/words/${word.id}/done`;
+  const body = { id: word.id, done: word.done };
+  console.log({ path, body });
+  const result = await executeApi(path, 'put', body);
+  if (result.status !== 'ok') {
+    throw new Error(`${result.status} ${result.data}`);
+  }
+  return result.data;
+}
+
+export async function deleteWord(id) {
+  const path = `/words/${id}`;
+  const result = await executeApi(path, 'delete');
+  if (result.status !== 'ok') {
+    throw new Error(`${result.status} ${result.data}`);
+  }
+  return result.data;
+}
+
+export async function translate(from, word) {
+  if (!word) return { status: 'ok', data: {} };
+  const path = `/translate/${from}/${encodeURIComponent(word)}`;
+  const result = await executeApi(path);
+  if (result.status !== 'ok') {
+    throw new Error(`${result.status} ${result.data}`);
+  }
+  return result.data;
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 const { Translate } = require('@google-cloud/translate').v2;
@@ -8,6 +10,13 @@ const translateClient = new Translate();
 const knex = require('./database');
 
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.json());
+
+console.log('process.env.NODE_ENV =', process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  console.log('Enabling CORS for development.');
+  app.use(cors());
+}
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
@@ -36,22 +45,60 @@ app.get('/words', async (req, res) => {
 
 // 単語追加
 app.post('/words', async (req, res) => {
-  res.json({ status: 'ok', data: [] });
+  const word = { ...req.body, done: 0 };
+  if (word.id !== undefined && word.id !== 0) {
+    res.status(400);
+    res.json({ status: 'error', data: 'Invalid id' });
+    return;
+  }
+  console.log(JSON.stringify(word));
+
+  const ids = await knex('words').insert(word, ['id']);
+  console.log({ ids });
+  const savedRows = await knex.select('*').from('words').where({ id: ids[0] });
+
+  res.json({ status: 'ok', data: savedRows[0] });
 });
 
 // 単語更新
 app.put('/words/:id', async (req, res) => {
-  res.json({ status: 'ok', data: [] });
+  const id = req.params.id | 0;
+  const word = { ...req.body };
+  if (id === undefined || id === 0 || id !== word.id) {
+    res.status(400);
+    res.json({ status: 'error', data: 'Invalid id' });
+    return;
+  }
+  console.log(JSON.stringify(word));
+
+  delete word.id;
+  await knex('words').where({ id }).update(word, ['id']);
+  const savedRows = await knex.select('*').from('words').where({ id });
+  res.json({ status: 'ok', data: savedRows[0] });
 });
 
 // 単語更新（doneフラグのみ）
 app.put('/words/:id/done', async (req, res) => {
-  res.json({ status: 'ok', data: [] });
+  const id = req.params.id | 0;
+  const word = { ...req.body };
+  if (id === undefined || id === 0 || id !== word.id) {
+    res.status(400);
+    res.json({ status: 'error', data: 'Invalid id' });
+    return;
+  }
+  console.log(JSON.stringify(word));
+
+  await knex('words').where({ id }).update({ done: word.done }, ['id']);
+  const savedRows = await knex.select('*').from('words').where({ id });
+  res.json({ status: 'ok', data: savedRows[0] });
 });
 
 // 単語削除
 app.delete('/words/:id', async (req, res) => {
-  res.json({ status: 'ok', data: [] });
+  const id = req.params.id | 0;
+  const rows = await knex('words').where({ id }).del();
+  console.log({ rows });
+  res.json({ status: 'ok', data: { deletedRows: rows } });
 });
 
 // 3ヶ国語自動翻訳
@@ -71,5 +118,5 @@ app.get('/translate/:from/:word', async (req, res) => {
     data[lang] = result[0];
   }
 
-  res.json(data);
+  res.json({ status: 'ok', data });
 });
